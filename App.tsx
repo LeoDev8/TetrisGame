ece1b6b4554121d12bfe7106e617b0779460ff59
LeoDev8 <zhihao_2023@163.com>
2024-06-19 22:20:35 +0800

    2024-06-19

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {backgroundColors as Colors} from './Maps/Colors';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import BtnArea from './components/BtnArea';
 import MainArea from './components/MainArea';
@@ -6,13 +7,27 @@ import ScoreArea from './components/ScoreArea';
 
 export default function App(): React.JSX.Element {
   return (
-    <SafeAreaView style={styles.container}>
+    // <SafeAreaView style={styles.container}>
+    //   <View style={styles.topDisplay}>
+    //     <MainArea></MainArea>
+    //     <ScoreArea></ScoreArea>
+    //   </View>
+    //   <BtnArea></BtnArea>
+    // </SafeAreaView>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Colors.gray,
+        height: '100%',
+      }}>
       <View style={styles.topDisplay}>
         <MainArea></MainArea>
         <ScoreArea></ScoreArea>
       </View>
       <BtnArea></BtnArea>
-    </SafeAreaView>
+    </View>
   );
 }
 
---
+++ Maps/Colors.js
@@ -0,0 +1,16 @@
+/**
+ * Export some beautiful colors for this app
+ */
+
+const backgroundColors = {
+  gray: '#E0DFD8',
+};
+
+const tetrisColors = {
+  black: '000',
+  green: '#83FF79',
+  blue: '#64F6FF',
+  yellow: '#FFE664',
+};
+
+export {backgroundColors, tetrisColors};

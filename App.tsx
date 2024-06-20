e2e7a2beb335cb9bf890671db803b35648ac456d
LeoDev8 <zhihao_2023@163.com>
2024-06-20 18:22:27 +0800

    restart

--- App.tsx
+++ App.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import {backgroundColors as Colors} from './Maps/Colors';
+import {backgroundColors as Colors} from './Maps/Settings';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import BtnArea from './components/BtnArea';
-import MainArea from './components/MainArea';
-import ScoreArea from './components/ScoreArea';
 
 export default function App(): React.JSX.Element {
   return (
-    // <SafeAreaView style={styles.container}>
-    //   <View style={styles.topDisplay}>
-    //     <MainArea></MainArea>
-    //     <ScoreArea></ScoreArea>
-    //   </View>
-    //   <BtnArea></BtnArea>
-    // </SafeAreaView>
     <View
       style={{
         flex: 1,
@@ -21,13 +11,7 @@ export default function App(): React.JSX.Element {
         alignItems: 'center',
         backgroundColor: Colors.gray,
         height: '100%',
-      }}>
-      <View style={styles.topDisplay}>
-        <MainArea></MainArea>
-        <ScoreArea></ScoreArea>
-      </View>
-      <BtnArea></BtnArea>
-    </View>
+      }}></View>
   );
 }
 
